04538f9dc9f487ebf0fea058a53a35915d88d87d
sochowski <aidansochowski64@gmail.com>
2025-02-01 15:12:49 -0800

    Color configs

---
+++ config.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Liberation Mono:size=14"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#b3b1ad", "#0d1117" },
+	[SchemeSel] = { "#FFFFFF", "#58a6ff" },
+	[SchemeOut] = { "#FFFFFF", "#6e7681" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
